0867dc50d1b89c0ab4ce9b2d1ce1c7dfaa6d088c
Nicolas Fränkel <nicolas@frankel.ch>
2017-04-10 22:10:15 +0200

    Use lambda with receiver into own extension function

--- src/main/kotlin/ch/frankel/kotlindsl/KotlinUI.kt
+++ src/main/kotlin/ch/frankel/kotlindsl/KotlinUI.kt
@@ -8,13 +8,17 @@ class KotlinUI : UI() {
 
     override fun init(request: VaadinRequest) {
         theme = "valo"
-        verticalLayout(spacing = true)
+        verticalLayout(spacing = true) {
+            addComponent(Label())
+        }
     }
 }
 
-fun UI.verticalLayout(spacing: Boolean = false, margin: Boolean = false) {
+fun UI.verticalLayout(spacing: Boolean = false,
+                      margin: Boolean = false,
+                      init: VerticalLayout.() -> Unit) {
     content = VerticalLayout().apply {
         this.isSpacing = spacing
         this.margin = MarginInfo(margin)
-    }
+    }.apply(init)
 }
