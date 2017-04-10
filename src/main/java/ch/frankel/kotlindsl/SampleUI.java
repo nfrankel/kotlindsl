242de2359a53861f8b7bddc90b348ee2cc234611
Nicolas Fränkel <nicolas@frankel.ch>
2017-04-10 21:26:26 +0200

    Introduce utility class to create layout

--- src/main/java/ch/frankel/kotlindsl/SampleUI.java
+++ src/main/java/ch/frankel/kotlindsl/SampleUI.java
@@ -12,11 +12,20 @@ public class SampleUI extends UI {
 
     protected void init(VaadinRequest request) {
         setTheme("valo");
-        VerticalLayout layout = new VerticalLayout();
+        VerticalLayout layout = UIUtils.verticalLayout(this);
         Label label = new Label("<strong>Hello</strong>", HTML);
         TextField field = new TextField("world!");
         layout.addComponents(label, field);
         layout.setMargin(true);
         setContent(layout);
+    }
+}
+
+class UIUtils {
+
+    static VerticalLayout verticalLayout(UI ui) {
+        VerticalLayout verticalLayout = new VerticalLayout();
+        ui.setContent(verticalLayout);
+        return verticalLayout;
     }
 }
