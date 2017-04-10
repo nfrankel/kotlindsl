c289643d7faf64f9086cf6a87725d114f7f1af2a
Nicolas Fränkel <nicolas@frankel.ch>
2017-04-10 21:16:40 +0200

    Add first Kotlin UI

---
+++ src/main/kotlin/ch/frankel/kotlindsl/KotlinUI.kt
@@ -0,0 +1,11 @@
+package ch.frankel.kotlindsl
+
+import com.vaadin.server.*
+import com.vaadin.ui.*
+
+class KotlinUI : UI() {
+
+    override fun init(request: VaadinRequest) {
+        setTheme("valo")
+    }
+}
