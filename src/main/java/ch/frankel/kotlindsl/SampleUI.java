97bea125615a1e094fb2c5df312a78fbd79319b2
Nicolas Fränkel <nicolas@frankel.ch>
2017-04-10 21:09:15 +0200

    Initial commit

---
+++ src/main/java/ch/frankel/kotlindsl/SampleUI.java
@@ -0,0 +1,23 @@
+package ch.frankel.kotlindsl;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+import static com.vaadin.shared.ui.ContentMode.HTML;
+
+public class SampleUI extends UI {
+
+    protected void init(VaadinRequest request) {
+        setTheme("valo");
+        VerticalLayout layout = new VerticalLayout();
+        Label label = new Label("<strong>Hello</strong>", HTML);
+        TextField field = new TextField("world!");
+        layout.addComponent(label);
+        layout.addComponent(field);
+        layout.setMargin(true);
+        setContent(layout);
+    }
+}
